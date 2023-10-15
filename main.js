faaddcb3333acd57627b2089310bd8ea62778493
Your Name <you@example.com>
2023-10-15 22:04:08 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+
+let a = 1
+if (a==1){
+    console.log('dushanbi')
+}else if(a==2){
+    console.log('seshanbi')
+}else if(a==3){
+    console.log('chorshanbi')
+}else if(a==4){
+    console.log('payshanba')
+}else if(a==5){
+    console.log('juma')
+}else if(a==6){
+    console.log('shanba')
+}else if(a==7){
+    console.log('yakshanba')
+}
+
+
+    //--part1--//
+
+
+let a = 15
+    if (a % 5 == 0){
+        console.log('5 ga bolinadi');
+    }else{
+        console.log('5 ga bolinmaydi');
+    }
+
+
+
+
+            //--part2--//
+
+let a = 55
+    if( a % 3 == 0){
+        console.log('3 ga bolinadi');
+    }else{
+        console.log('3 ga bolinmaydi');
+    }
+
+
+        //--part3--//
+
+let a = 23
+    if (a % 3 == 0){
+    if (a % 5 == 0){
+        console.log('3 bilan 5 ga bolinsin');
+    }else{
+        console.log('3 bilan 5 ga bolinsin');
+    }
+    }else{
+        console.log('3 bilan 5 ga bolinsin');
+    }
